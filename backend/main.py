3ac1bed477b4d9c33775b62429cad7c39eba9b98
miraclis <mihail.allo3065@gmail.com>
2026-09-07 20:57:37 +0200

    register

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,29 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from pwdlib import PasswordHash
+
 
 import models
 from database import SessionLocal
 
 app = FastAPI()
 
+password_hash = PasswordHash.recommended()
+
 
 class RegisterRequest(BaseModel):
     email: str
     password: str
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -19,11 +32,32 @@ def root():
 
 
 @app.post("/register")
-def register(data: RegisterRequest):
+def register(data: RegisterRequest, db: Session = Depends(get_db)):
+    existing_user = db.query(models.User).filter(
+        models.User.email == data.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    hashed_password = password_hash.hash(data.password)
+
+    new_user = models.User(
+        email=data.email,
+        password_hash=hashed_password,
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
     return {
         "message": "User registered",
-        "email": data.email
+        "id": new_user.id,
+        "email": new_user.email,
+        "role": new_user.role,
+        "status": new_user.status,
     }
-    
-
-
